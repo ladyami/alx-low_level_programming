9f1c9eef7f10bcfd0c9a95421048101e633829f3
ladyami <aminaoyegoke6@gmail.com>
2023-02-16 12:03:23 -0800

    a C program that prints

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * main - is the entry point
+ * Return: if return 0 mean success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
