a1d48f468543eafd39a2befbbc33facfa2200db4
ladyami <aminaoyegok6@gmail.com>
2023-04-12 02:06:36 -0700

    fix error

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,17 +1,22 @@
 #include "main.h"
 
-/**
-* print_binary - Prints the binary equivalent of a decimal number
-* @n: Number to print in binary
-*/
 void print_binary(unsigned long int n)
 {
-int i;
-for (i = 63; i >= 0; i--)
-{
-if (n & (1 << i))
+int i, count = 0;
+unsigned long int current;
+
+for (i = 63; i >= 0; i--) {
+current = n >> i;
+
+if (current & 1) {
 _putchar('1');
-else
+count++;
+} else if (count) {
+_putchar('0');
+}
+}
+
+if (!count) {
 _putchar('0');
 }
 }
